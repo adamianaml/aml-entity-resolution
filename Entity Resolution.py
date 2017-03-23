537f7570b1a5c6e2798fc7b93174d72e7b721e9c
ionox0 <ionox0@gmail.com>
2017-03-23 11:55:53 -0400

    start in common

--- Entity Resolution.py
+++ Entity Resolution.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[235]:
+# In[ ]:
 
 import pandas as pd
 import numpy as np
@@ -10,7 +10,7 @@ from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 
-# In[248]:
+# In[ ]:
 
 ama = pd.read_csv('./data/amazon.csv')
 rt = pd.read_csv('./data/rotten_tomatoes.csv')
@@ -20,12 +20,7 @@ test = pd.read_csv('./data/test.csv')
 train = pd.read_csv('./data/train.csv')
 
 
-# In[256]:
-
-train
-
-
-# In[222]:
+# In[ ]:
 
 ama.head()
 a = pd.DataFrame(ama[ama['id'] == 4])
@@ -38,7 +33,7 @@ ama.columns = [
 ]
 
 
-# In[221]:
+# In[ ]:
 
 b = pd.DataFrame(rt[rt['id'] == 3668])
 rt.columns = [
@@ -62,12 +57,12 @@ rt.columns = [
 ]
 
 
-# In[225]:
+# In[ ]:
 
 new_data = pd.DataFrame(dtype=str)
 
 
-# In[227]:
+# In[ ]:
 
 # Merge records from ama and rt
 for index, row in train.iterrows():
@@ -82,18 +77,43 @@ for index, row in train.iterrows():
     new_data = pd.concat([new_data, new_row])
 
 
-# In[246]:
+# In[ ]:
 
+# Same director column
 new_data['directors_same'] = (new_data['director_left'] == new_data['director_right']).astype(int)
 
+# Split actors into unique columns
 actors_split = new_data['star_left'].str.split(',', expand=True)
 actors_split_columns = ['star_' + str(i) for i in range(len(actors_split.columns))]
 actors_split.columns = actors_split_columns
 new_data = pd.concat([new_data, actors_split], axis=1)
 
+star_columns = [
+    'star1_right',
+    'star2_right',
+    'star3_right',
+    'star4_right',
+    'star5_right',
+    'star6_right',
+]
+
+# Compute common actor count column
+new_data['common_actor_count'] = pd.Series()
+
+for i, row in new_data.iterrows():
+    actors_left = set(row[actors_split_columns])
+    actors_left.discard(None)
+    actors_right = set(row[star_columns])
+    actors_right.discard(None)
+    
+    new_data.iloc[i, new_data.columns.get_loc('common_actor_count')] = len(actors_left.intersection(actors_right))
+#     print(new_data.iloc[i, new_data.columns.get_loc('common_actor_count')])
 
 
-new_data['same_actor_count'] = len()
+# In[ ]:
+
+pd.options.display.max_columns = 999
+new_data['common_actor_count']
 
 
 # In[ ]:
@@ -108,7 +128,7 @@ for i, row in train.iterrows():
 [ 1 for i, row in train.iterrows() if ama[ama['id_left'] == row['id1'] == rtrow[rtrow['id_right'] == row['id 2']] else 0 ]
 
 
-# In[234]:
+# In[ ]:
 
 clf = RandomForestClassifier()
 x_train, x_test, y_train, y_test = train_test_split(new_data.ix[:, new_data.columns != 'gold'], new_data.iloc[:,-1])
