b23d9b310b8c65f53e2098994d63bb33a4cdf4e6
ionox0 <ionox0@gmail.com>
2017-03-24 16:39:26 -0400

    Final Classifier py and ipynb

--- Final Classifier.py
+++ Final Classifier.py
@@ -1,32 +1,41 @@
 
 # coding: utf-8
 
-# In[843]:
+# # Amazon and Rotten Tomatoes Entity Resolution
+# ### Adam Coviensky (ac4092), Ian Johnson (icj2103)
+# ### Instabase submission: icj2103
+
+# Our entity resolution technique involved joining the Amazon and Rotten Tomatoes databases for the ID’s listed in the training set. We then matched the schemas for the features contained in both of the datasets and created new features from these. These features included the absolute value of the difference in time. Boolean features indicating whether or not the directors of the two movies are the same, the runtimes are the same, and whether or not the runtimes are the same within some range (we used 3 minutes). We then created features for the number of matching actors and for the percentage of matching actors. Finally, we performed cross-validation with a customized gridsearch using the training set to determine which features we should be keeping in our final model and which classifier to use between a decision tree, a random forest, and the gradient boosting classifier.
+#  
+# Our model had a precision, recall, and F1-scores of 0.97, 0.97 and 0.96 respectively for the training set. We obtained these scores by using train_test_split on our training set and fitting the model to the training portion and testing it on the testing portion.
+#  
+#  
+# The most important features were determined to be ‘time_same_2’ which is a binary variable indicating whether the two runtimes are equivalent within a range of 3 minutes. Also, ‘directors_same’, indicated whether the two directors are the same.  ‘num_match_stars’ and ‘percent_match_stars’ indicated the number of matching stars and the percentage of the stars matched from the Amazon dataset respectively. These were the features we used to train our final model.
+#  
+# We avoided pairwise comparison of all movies across both datasets by simply joining the id’s listed in the training set to create a model. We trained a random forest classifier on this training data with our newly created features. This allowed us to predict whether two new movies matched using our trained random forest on a new combination of two movies.
+
+# In[18]:
 
 import re
 import pandas as pd
 import numpy as np
 
 from sklearn.cross_validation import train_test_split
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
-pd.options.display.max_columns = 999
-
-
-# ### Load Data
-
-# In[844]:
+np.random.seed(42)
 
 ama = pd.read_csv('./data/amazon.csv')
 rt = pd.read_csv('./data/rotten_tomatoes.csv')
 
-holdout = pd.read_csv('./data/holdout.csv')
-test = pd.read_csv('./data/test.csv')
-train = pd.read_csv('./data/train.csv')
+holdout = pd.read_csv('./holdout.csv')
+test = pd.read_csv('./test.csv')
+train = pd.read_csv('./train.csv')
 
 
-# In[845]:
+# ### Define Columns
+
+# In[19]:
 
 ama.columns = [
     'id_left',
@@ -57,13 +66,13 @@ rt.columns = [
 ]
 
 
-# ### Merge Ama + Rt and Create Features
+# ### Feature Creation Function
 
-# In[1037]:
+# In[20]:
 
 def create_features(dataset):
     new_data = pd.DataFrame(dtype=str)
-    
+
     for index, row in dataset.iterrows():
         amarow = ama[ama['id_left'] == row['id1']]
         rtrow = rt[rt['id_right'] == row['id 2']]
@@ -73,13 +82,13 @@ def create_features(dataset):
 
         new_row = pd.concat([amarow, rtrow], axis=1)
         new_data = pd.concat([new_data, new_row])
-    
+
     new_data.fillna('0', inplace = True)
     new_data.dropna()
-    
+
     # Compute directors match column
     new_data['directors_same'] = (new_data['director_left'] == new_data['director_right']).astype(int)
-    
+
     # Compute time columns
     new_data['time_left'] = new_data['time_left'].astype(str)
     new_data['time_right'] = new_data['time_right'].astype(str)
@@ -89,40 +98,40 @@ def create_features(dataset):
     new_data['time_same_2'] = (abs((new_data['time_norm_left'].astype(int) - new_data['time_norm_right'].astype(int)).astype(int)) <= 3).astype(int)
     new_data['time_diff'] = (new_data['time_norm_left'].astype(int) - new_data['time_norm_right'].astype(int)).astype(int)
     new_data['time_diff_abs'] = abs(new_data['time_diff']).astype(int)
-    
+
     # Compute actors columns
     actors_split = new_data['star_left'].str.split(', ', expand=True)
     for i in range(6 - actors_split.shape[1]):
         actors_split[str(i)] = ""
     actors_split.columns = ['star_' + str(i) for i in range(6)]
     new_data = pd.concat([new_data, actors_split], axis=1)
-    
+
     # Number actors match
     cols = list(new_data.loc[:,'star_0':'star_4']) + list(new_data.loc[:,'star1_right':'star6_right'])
     new_data['num_match_stars'] = new_data[cols].apply(compute_number_stars_match, axis = 1)
-    
+
     # Percent actors match
     new_data['percent_match_stars'] = new_data[cols].apply(compute_percent_stars_match, axis = 1)
-    
+
     return new_data
 
 
-# In[929]:
+# ### Helper Feature Creation Functions
+
+# In[21]:
 
 def compute_number_stars_match(row):
     actors_left = ['star_0', 'star_1', 'star_2', 'star_3', 'star_4']
     actors_right = ['star1_right', 'star2_right', 'star3_right', 'star4_right', 'star5_right', 'star6_right']
     list_left = row.loc[actors_left].tolist()
     list_right = row.loc[actors_right].tolist()
-    
+
     # Avoid matching nulls
     list_left = filter(None, list_left)
     list_right = filter(None, list_right)
-    
+
     return len(np.intersect1d(list_left, list_right))
 
-
-# In[930]:
 
 def compute_percent_stars_match(row):
     actors_left = ['star_0', 'star_1', 'star_2', 'star_3', 'star_4']
@@ -135,18 +144,16 @@ def compute_percent_stars_match(row):
     return x / ama_num
 
 
-# In[904]:
-
 regex = re.compile(r'[0-9]*')
 
 def compute_time_norm(row):
     row = str(row)
-    
+
     if '/' in row:
         # Invalid time entry
         print(row)
         return 0
-    
+
     match = regex.findall(row)
     temp = filter(None, match)
     if len(temp) == 2:
@@ -155,14 +162,12 @@ def compute_time_norm(row):
         return temp[0]
 
 
-# In[1001]:
-
 def remove_bad_samples(data):
-    
+
     train.reset_index(drop=True, inplace=True)
     data.reset_index(drop=True, inplace=True)
     data = pd.concat([data, train], axis=1)
-    
+
     # Remove bad training rows
     data = data[data['id_left'] != 199]
     data = data[data['id_left'] != 680]
@@ -170,38 +175,29 @@ def remove_bad_samples(data):
     data = data[data['id_left'] != 756]
     data = data[data['id_left'] != 770]
     data = data[data['id_left'] != 1701]
-    
+
     return data
 
 
-# ### Predict with train
+# ### Predict With Training Data
 
-# In[1142]:
+# In[23]:
 
 features_cols = [
-#     'time_diff',
-#     'time_same',
     'time_same_2',
-#     'time_diff_abs',
     'directors_same',
     'num_match_stars',
     'percent_match_stars',
-#     'year_right',
-#     'rotten_tomatoes_right',
-#     'audience_rating_right'
 ]
 
 train_data = create_features(train)
 train_data = remove_bad_samples(train_data)
 
-x_train, x_test, y_train, y_test = train_test_split(train_data[features_cols], train_data['gold'])
+x_train, x_test, y_train, y_test = train_test_split(train_data[features_cols], train_data['gold'], random_state=10)
 
-clf = RandomForestClassifier()
+clf = RandomForestClassifier(random_state=42)
 clf.fit(x_train, y_train)
 scores = clf.score(x_test, y_test)
-
-
-# In[1145]:
 
 preds = clf.predict(x_test)
 
@@ -209,26 +205,20 @@ preds = clf.predict(x_test)
 from sklearn.metrics import classification_report
 print(classification_report(y_test, preds))
 
-# Missed preds:
-errors = preds - y_test
-error_ind = np.nonzero(errors)[0]
 
-print("Error inds: " + str(error_ind))
-print("Preds: " + str(preds[error_ind]))
-print("Labels: " + str(y_test.as_matrix()[error_ind]))
+# ### Final Predictions
 
-rel_cols = ['director_left', 'time_right', 'directors_same', 'time_same', 'time_diff', 'num_match_stars', 'gold']
-x_test.iloc[error_ind]
+# In[24]:
 
-# Conf matrix:
-# from sklearn.metrics import confusion_matrix
-# confusion_matrix(y_test, preds)
+features_cols = [
+    'time_same_2',
+    'directors_same',
+    'num_match_stars',
+    'percent_match_stars',
+]
 
 
-# ### Predict with test
-
-# In[1138]:
-
+# Test Predictions
 train_data = create_features(train)
 train_data = remove_bad_samples(train_data)
 test_data = create_features(test)
@@ -237,18 +227,12 @@ clf = RandomForestClassifier()
 clf.fit(train_data[features_cols], train_data['gold'])
 test_preds = clf.predict(test_data[features_cols])
 
-
-# In[1139]:
-
 test_preds = pd.DataFrame(test_preds)
 test_preds.columns = ['gold']
 test_preds.to_csv('test_gold.csv', index=False)
 
 
-# ### Predict with Holdout
-
-# In[1140]:
-
+# Holdout Predictions
 train_data = create_features(train)
 train_data = remove_bad_samples(train_data)
 holdout_data = create_features(holdout)
@@ -257,88 +241,9 @@ clf = RandomForestClassifier()
 clf.fit(train_data[features_cols], train_data['gold'])
 holdout_preds = clf.predict(holdout_data[features_cols])
 
-
-# In[1141]:
-
 holdout_preds = pd.DataFrame(holdout_preds)
 holdout_preds.columns = ['gold']
 holdout_preds.to_csv('holdout_gold.csv', index=False)
-
-
-# ### Try all combinations of features / classifiers / removing bad data
-
-# In[1066]:
-
-import itertools
-from sklearn.cross_validation import cross_val_score
-
-features = [
-    'directors_same',
-]
-
-features_opt = [
-    'time_same_2',
-    'time_diff_abs',
-    'num_match_stars',
-    'percent_match_stars',
-]
-
-remove_bad_data = [True, False]
-
-classifiers = [
-#     DecisionTreeClassifier(),
-    RandomForestClassifier(),
-    GradientBoostingClassifier()
-]
-
-results = []
-
-for r in range(2, len(features_opt)):
-    for feature_comb in itertools.combinations(features_opt, r):
-        for remove_bd in remove_bad_data:
-            for clf in classifiers:
-                
-                train_data = create_features(train)
-                
-                if remove_bd:
-                    train_data = remove_bad_samples(train_data)
-
-                x_train, x_test, y_train, y_test = train_test_split(train_data[list(feature_comb) + features], train_data['gold'])
-
-                clf.fit(x_train, y_train)
-                scores = cross_val_score(clf, x_test, y_test)
-                
-                res = [r, feature_comb, remove_bd, type(clf).__name__, scores.mean()]
-                print(res)
-                results.append(res)
-
-
-# In[1067]:
-
-results_csv = pd.DataFrame(results)
-results_csv.columns = ['r', 'features', 'remove_bd', 'clf', 'scores']
-results_csv.to_csv('choosing_final_classifier.csv')
-
-
-# In[1070]:
-
-np.unique(results_csv['scores'])
-
-
-# ### Testing best results from^
-
-# In[1134]:
-
-final_feats = ['directors_same', 'time_same_2', 'num_match_stars', 'percent_match_stars']
-
-train_data = create_features(train)
-train_data = remove_bad_samples(train_data)
-
-x_train, x_test, y_train, y_test = train_test_split(train_data[final_feats], train_data['gold'])
-
-RandomForestClassifier().fit(x_train, y_train)
-scores = cross_val_score(clf, x_test, y_test, cv=3)
-scores
 
 
 # In[ ]:
--- final_classifier.py
+++ final_classifier.py
@@ -1,23 +1,12 @@
-
-# coding: utf-8
-
-# In[843]:
-
 import re
 import pandas as pd
 import numpy as np
 
 from sklearn.cross_validation import train_test_split
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.tree import DecisionTreeClassifier
-
-pd.options.display.max_columns = 999
+from sklearn.ensemble import RandomForestClassifier
 
 
-# ### Load Data
-
-# In[844]:
-
+# Load Data
 ama = pd.read_csv('./data/amazon.csv')
 rt = pd.read_csv('./data/rotten_tomatoes.csv')
 
@@ -26,8 +15,7 @@ test = pd.read_csv('./data/test.csv')
 train = pd.read_csv('./data/train.csv')
 
 
-# In[845]:
-
+# Define Columns
 ama.columns = [
     'id_left',
     'time_left',
@@ -57,13 +45,10 @@ rt.columns = [
 ]
 
 
-# ### Merge Ama + Rt and Create Features
-
-# In[1037]:
-
+# Feature Creation
 def create_features(dataset):
     new_data = pd.DataFrame(dtype=str)
-    
+
     for index, row in dataset.iterrows():
         amarow = ama[ama['id_left'] == row['id1']]
         rtrow = rt[rt['id_right'] == row['id 2']]
@@ -73,13 +58,13 @@ def create_features(dataset):
 
         new_row = pd.concat([amarow, rtrow], axis=1)
         new_data = pd.concat([new_data, new_row])
-    
+
     new_data.fillna('0', inplace = True)
     new_data.dropna()
-    
+
     # Compute directors match column
     new_data['directors_same'] = (new_data['director_left'] == new_data['director_right']).astype(int)
-    
+
     # Compute time columns
     new_data['time_left'] = new_data['time_left'].astype(str)
     new_data['time_right'] = new_data['time_right'].astype(str)
@@ -89,40 +74,37 @@ def create_features(dataset):
     new_data['time_same_2'] = (abs((new_data['time_norm_left'].astype(int) - new_data['time_norm_right'].astype(int)).astype(int)) <= 3).astype(int)
     new_data['time_diff'] = (new_data['time_norm_left'].astype(int) - new_data['time_norm_right'].astype(int)).astype(int)
     new_data['time_diff_abs'] = abs(new_data['time_diff']).astype(int)
-    
+
     # Compute actors columns
     actors_split = new_data['star_left'].str.split(', ', expand=True)
     for i in range(6 - actors_split.shape[1]):
         actors_split[str(i)] = ""
     actors_split.columns = ['star_' + str(i) for i in range(6)]
     new_data = pd.concat([new_data, actors_split], axis=1)
-    
+
     # Number actors match
     cols = list(new_data.loc[:,'star_0':'star_4']) + list(new_data.loc[:,'star1_right':'star6_right'])
     new_data['num_match_stars'] = new_data[cols].apply(compute_number_stars_match, axis = 1)
-    
+
     # Percent actors match
     new_data['percent_match_stars'] = new_data[cols].apply(compute_percent_stars_match, axis = 1)
-    
+
     return new_data
 
 
-# In[929]:
-
+# Helper Functions
 def compute_number_stars_match(row):
     actors_left = ['star_0', 'star_1', 'star_2', 'star_3', 'star_4']
     actors_right = ['star1_right', 'star2_right', 'star3_right', 'star4_right', 'star5_right', 'star6_right']
     list_left = row.loc[actors_left].tolist()
     list_right = row.loc[actors_right].tolist()
-    
+
     # Avoid matching nulls
     list_left = filter(None, list_left)
     list_right = filter(None, list_right)
-    
+
     return len(np.intersect1d(list_left, list_right))
 
-
-# In[930]:
 
 def compute_percent_stars_match(row):
     actors_left = ['star_0', 'star_1', 'star_2', 'star_3', 'star_4']
@@ -135,18 +117,16 @@ def compute_percent_stars_match(row):
     return x / ama_num
 
 
-# In[904]:
-
 regex = re.compile(r'[0-9]*')
 
 def compute_time_norm(row):
     row = str(row)
-    
+
     if '/' in row:
         # Invalid time entry
         print(row)
         return 0
-    
+
     match = regex.findall(row)
     temp = filter(None, match)
     if len(temp) == 2:
@@ -155,14 +135,12 @@ def compute_time_norm(row):
         return temp[0]
 
 
-# In[1001]:
-
 def remove_bad_samples(data):
-    
+
     train.reset_index(drop=True, inplace=True)
     data.reset_index(drop=True, inplace=True)
     data = pd.concat([data, train], axis=1)
-    
+
     # Remove bad training rows
     data = data[data['id_left'] != 199]
     data = data[data['id_left'] != 680]
@@ -170,178 +148,48 @@ def remove_bad_samples(data):
     data = data[data['id_left'] != 756]
     data = data[data['id_left'] != 770]
     data = data[data['id_left'] != 1701]
-    
+
     return data
 
 
-# ### Predict with train
-
-# In[1142]:
-
-features_cols = [
-#     'time_diff',
-#     'time_same',
-    'time_same_2',
-#     'time_diff_abs',
-    'directors_same',
-    'num_match_stars',
-    'percent_match_stars',
-#     'year_right',
-#     'rotten_tomatoes_right',
-#     'audience_rating_right'
-]
-
-train_data = create_features(train)
-train_data = remove_bad_samples(train_data)
-
-x_train, x_test, y_train, y_test = train_test_split(train_data[features_cols], train_data['gold'])
-
-clf = RandomForestClassifier()
-clf.fit(x_train, y_train)
-scores = clf.score(x_test, y_test)
 
 
-# In[1145]:
+if __name__ == '__main__':
 
-preds = clf.predict(x_test)
+	features_cols = [
+		'time_same_2',
+		'directors_same',
+		'num_match_stars',
+		'percent_match_stars',
+	]
 
-# F1 score:
-from sklearn.metrics import classification_report
-print(classification_report(y_test, preds))
+	# Test Predictions
+	train_data = create_features(train)
+	train_data = remove_bad_samples(train_data)
+	test_data = create_features(test)
 
-# Missed preds:
-errors = preds - y_test
-error_ind = np.nonzero(errors)[0]
+	clf = RandomForestClassifier()
+	clf.fit(train_data[features_cols], train_data['gold'])
+	test_preds = clf.predict(test_data[features_cols])
 
-print("Error inds: " + str(error_ind))
-print("Preds: " + str(preds[error_ind]))
-print("Labels: " + str(y_test.as_matrix()[error_ind]))
-
-rel_cols = ['director_left', 'time_right', 'directors_same', 'time_same', 'time_diff', 'num_match_stars', 'gold']
-x_test.iloc[error_ind]
-
-# Conf matrix:
-# from sklearn.metrics import confusion_matrix
-# confusion_matrix(y_test, preds)
-
-
-# ### Predict with test
-
-# In[1138]:
-
-train_data = create_features(train)
-train_data = remove_bad_samples(train_data)
-test_data = create_features(test)
-
-clf = RandomForestClassifier()
-clf.fit(train_data[features_cols], train_data['gold'])
-test_preds = clf.predict(test_data[features_cols])
+	test_preds = pd.DataFrame(test_preds)
+	test_preds.columns = ['gold']
+	test_preds.to_csv('test_gold.csv', index=False)
 
 
-# In[1139]:
+	# Holdout Predictions
+	train_data = create_features(train)
+	train_data = remove_bad_samples(train_data)
+	holdout_data = create_features(holdout)
 
-test_preds = pd.DataFrame(test_preds)
-test_preds.columns = ['gold']
-test_preds.to_csv('test_gold.csv', index=False)
+	clf = RandomForestClassifier()
+	clf.fit(train_data[features_cols], train_data['gold'])
+	holdout_preds = clf.predict(holdout_data[features_cols])
 
+	holdout_preds = pd.DataFrame(holdout_preds)
+	holdout_preds.columns = ['gold']
+	holdout_preds.to_csv('holdout_gold.csv', index=False)
 
-# ### Predict with Holdout
-
-# In[1140]:
-
-train_data = create_features(train)
-train_data = remove_bad_samples(train_data)
-holdout_data = create_features(holdout)
-
-clf = RandomForestClassifier()
-clf.fit(train_data[features_cols], train_data['gold'])
-holdout_preds = clf.predict(holdout_data[features_cols])
-
-
-# In[1141]:
-
-holdout_preds = pd.DataFrame(holdout_preds)
-holdout_preds.columns = ['gold']
-holdout_preds.to_csv('holdout_gold.csv', index=False)
-
-
-# ### Try all combinations of features / classifiers / removing bad data
-
-# In[1066]:
-
-import itertools
-from sklearn.cross_validation import cross_val_score
-
-features = [
-    'directors_same',
-]
-
-features_opt = [
-    'time_same_2',
-    'time_diff_abs',
-    'num_match_stars',
-    'percent_match_stars',
-]
-
-remove_bad_data = [True, False]
-
-classifiers = [
-#     DecisionTreeClassifier(),
-    RandomForestClassifier(),
-    GradientBoostingClassifier()
-]
-
-results = []
-
-for r in range(2, len(features_opt)):
-    for feature_comb in itertools.combinations(features_opt, r):
-        for remove_bd in remove_bad_data:
-            for clf in classifiers:
-                
-                train_data = create_features(train)
-                
-                if remove_bd:
-                    train_data = remove_bad_samples(train_data)
-
-                x_train, x_test, y_train, y_test = train_test_split(train_data[list(feature_comb) + features], train_data['gold'])
-
-                clf.fit(x_train, y_train)
-                scores = cross_val_score(clf, x_test, y_test)
-                
-                res = [r, feature_comb, remove_bd, type(clf).__name__, scores.mean()]
-                print(res)
-                results.append(res)
-
-
-# In[1067]:
-
-results_csv = pd.DataFrame(results)
-results_csv.columns = ['r', 'features', 'remove_bd', 'clf', 'scores']
-results_csv.to_csv('choosing_final_classifier.csv')
-
-
-# In[1070]:
-
-np.unique(results_csv['scores'])
-
-
-# ### Testing best results from^
-
-# In[1134]:
-
-final_feats = ['directors_same', 'time_same_2', 'num_match_stars', 'percent_match_stars']
-
-train_data = create_features(train)
-train_data = remove_bad_samples(train_data)
-
-x_train, x_test, y_train, y_test = train_test_split(train_data[final_feats], train_data['gold'])
-
-RandomForestClassifier().fit(x_train, y_train)
-scores = cross_val_score(clf, x_test, y_test, cv=3)
-scores
-
-
-# In[ ]:
 
 
 
